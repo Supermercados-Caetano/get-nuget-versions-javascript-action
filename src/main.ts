e6ba760cf8ef6af2dc95bce18f552081256ff112
Guilherme Simões <guilherme@smcaetano.com.br>
2022-03-23 13:04:52 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function run() {
 		const password = core.getInput('password');
 
 		const resp = await axios.get<PackageResponse>(url, { headers: { 'Authorization': 'Basic ' + Buffer.from(username + ':' + password).toString('base64') } });
-		//console.log(JSON.stringify(resp));
+		console.log('resp', resp);
 		console.log(resp.data);
 		core.setOutput('versions', resp.data.versions);
 	} catch (error: any) {
