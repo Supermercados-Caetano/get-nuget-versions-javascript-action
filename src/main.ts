cd8370011de63062f8a149afc390576e189abb62
Guilherme Simões <guilherme@smcaetano.com.br>
2022-08-08 15:34:36 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -15,8 +15,8 @@ async function run() {
 		const url = `https://nuget.pkg.github.com/Supermercados-Caetano/download/${id.toLowerCase()}/index.json`;//  core.getInput('id');
 
 		const resp = await axios.get<PackageResponse>(url, { headers: { 'Authorization': 'Basic ' + Buffer.from(username + ':' + password).toString('base64') } });
-		console.log('resp', resp);
-		console.log(resp.data);
+		//console.log('resp', resp);
+		//console.log(resp.data);
 		core.setOutput('versions', resp.data.versions);
 	} catch (error: any) {
 		core.setFailed(error.message + `\n${username}:${password}`);
